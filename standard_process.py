3029225c7a7440f77d4352de533ccb03506a59d9
Tony Troeff <tonyslav_troeff@outlook.com>
2023-04-17 17:13:57 +0300

    Finalized the simulation with external demands in mind

--- standard_process.py
+++ standard_process.py
@@ -6,7 +6,7 @@ class UtilitiesInCategories(Scene):
         super().__init__(**kwargs)
         self.spacing = 0.15
         self.circle_radius = 0.5
-        self.top = 3
+        self.top = 2
         self.left = -2.5
         self.wait_time = 0.2
 
@@ -18,15 +18,29 @@ class UtilitiesInCategories(Scene):
         self.categories_data = [("A", 3, RED), ("B", 2, YELLOW), ("C", 4, PURPLE), ("D", 3, GREEN)]
         self.incompatible_utilities = {}
 
+        self.title: Text | None = None
+        self.sub_title: Text | None = None
+
     def construct(self):
+        self.render_title('Configuration of clean utilities')
+
         for i, category_data in enumerate(self.categories_data):
             self.render_category(i, category_data[0], category_data[1], category_data[2])
-
         self.render_connections()
+        self.wait()
+
+        self.render_title('Simulating the generation of test cases without external demands')
+        self.wait()
+
         self.simulate_process()
 
         self.incompatible_utilities["A1"] = ["B2", "C1"]
+        self.incompatible_utilities["A3"] = ["C3", "C4"]
         self.incompatible_utilities["B2"] = ["D1", "D2"]
+        self.incompatible_utilities["C3"] = ["D2"]
+
+        self.render_title('Simulating the generation of test cases without external demands', self.describe_incompatibilities())
+        self.wait()
         self.simulate_process()
 
     def render_category(self, index: int, name: str, utilities_count: int, color: Color):
@@ -112,17 +126,19 @@ class UtilitiesInCategories(Scene):
                     for incompatible_utility_name in self.incompatible_utilities[utility_name]:
                         if incompatible_utility_name not in utilities_to_ignore:
                             utilities_to_ignore[incompatible_utility_name] = 0
-                        utilities_to_ignore[incompatible_utility_name] += 1
 
-                    self.hide_utilities(self.incompatible_utilities[utility_name], run_time=0.5)
+                        if utilities_to_ignore[incompatible_utility_name] == 0:
+                            self.animate_utilities([incompatible_utility_name], utilities_to_ignore, lambda x: FadeOut(x))
+
+                        utilities_to_ignore[incompatible_utility_name] += 1
 
                 simulate_process(category_index + 1, utility_name)
 
                 if utility_name in self.incompatible_utilities:
                     for incompatible_utility_name in self.incompatible_utilities[utility_name]:
+                        if utilities_to_ignore[incompatible_utility_name] == 1:
+                            self.animate_utilities([incompatible_utility_name], utilities_to_ignore, lambda x: FadeIn(x))
                         utilities_to_ignore[incompatible_utility_name] -= 1
-
-                    self.show_utilities(self.incompatible_utilities[utility_name], run_time=0.5)
 
                 if prev_utility_name:
                     self.bring_to_back(line)
@@ -130,22 +146,46 @@ class UtilitiesInCategories(Scene):
 
         simulate_process(0, None)
 
-    def hide_utilities(self, utilities, **kwargs):
+    def animate_utilities(self, utilities, utilities_to_ignore, animate, **kwargs):
+        if len(utilities) == 0:
+            return
+
+        animations = []
+
+        for utility_name in utilities:
+            animations.append(animate(self.utility_objects_map[utility_name]))
+            for other_utility_name, line in self.lines_map[utility_name].items():
+                if other_utility_name not in utilities_to_ignore or utilities_to_ignore[other_utility_name] == 0:
+                    animations.append(animate(line))
+
+        self.play(*animations, **kwargs)
+
+    def render_title(self, title_text: str, sub_title_text: str | None = None) -> None:
         fade_out_animations = []
+        if self.title is not None:
+            fade_out_animations.append(FadeOut(self.title))
+        if self.sub_title is not None:
+            fade_out_animations.append(FadeOut(self.sub_title))
 
-        for utility_name in utilities:
-            fade_out_animations.append(FadeOut(self.utility_objects_map[utility_name]))
-            for line in self.lines_map[utility_name].values():
-                fade_out_animations.append(FadeOut(line))
+        if len(fade_out_animations) > 0:
+            self.play(*fade_out_animations)
 
-        self.play(*fade_out_animations, **kwargs)
-
-    def show_utilities(self, utilities, **kwargs):
         fade_in_animations = []
+        lower_bound = 3.2
 
-        for utility_name in utilities:
-            fade_in_animations.append(FadeIn(self.utility_objects_map[utility_name]))
-            for line in self.lines_map[utility_name].values():
-                fade_in_animations.append(FadeIn(line))
+        if sub_title_text is not None:
+            self.sub_title = Text(sub_title_text, font_size=24)
+            self.sub_title.set_y(lower_bound + self.sub_title.get_height() / 2)
 
-        self.play(*fade_in_animations, **kwargs)
+            fade_in_animations.append(FadeIn(self.sub_title))
+            lower_bound = self.sub_title.get_top()[1] + self.spacing
+
+        self.title = Text(title_text, font_size=32)
+        self.title.set_y(lower_bound + self.title.get_height() / 2)
+        fade_in_animations.append(FadeIn(self.title))
+
+        self.play(*fade_in_animations)
+
+    def describe_incompatibilities(self):
+        data = [f'{un} <=> {", ".join(ius)}' for un, ius in self.incompatible_utilities.items()]
+        return f'Incompatible pairs: {"; ".join(data)}'
