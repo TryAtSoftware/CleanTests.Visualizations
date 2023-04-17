5cf3785cc0a4a79b85a5b69b6d6ae6013bde3d7c
Tony Troeff <tonyslav_troeff@outlook.com>
2023-04-17 14:46:45 +0300

    Simulated the actual process of combining clean utilities

--- standard_process.py
+++ standard_process.py
@@ -13,6 +13,8 @@ class UtilitiesInCategories(Scene):
         self.utility_objects_map = {}
         self.utilities_by_category = {}
 
+        self.lines_map = {}
+
         self.categories_data = [("A", 3, RED), ("B", 2, YELLOW), ("C", 4, PURPLE), ("D", 3, GREEN)]
 
     def construct(self):
@@ -20,6 +22,7 @@ class UtilitiesInCategories(Scene):
             self.render_category(i, category_data[0], category_data[1], category_data[2])
 
         self.render_connections()
+        self.simulate_process(0, None)
 
     def render_category(self, index: int, name: str, utilities_count: int, color: Color):
         category_surface = Rectangle(
@@ -64,10 +67,35 @@ class UtilitiesInCategories(Scene):
             second_category_name = self.categories_data[i + 1][0]
 
             for first_utility_name in self.utilities_by_category[first_category_name]:
+                self.lines_map[first_utility_name] = {}
                 current_lines = []
+
                 for second_utility_name in self.utilities_by_category[second_category_name]:
-                    line = Line(self.utility_objects_map[first_utility_name].get_bottom(), self.utility_objects_map[second_utility_name].get_top())
-                    line.set_stroke(WHITE, width=2)
+                    line = Line(self.utility_objects_map[first_utility_name].get_bottom() + (self.spacing / 4) * DOWN, self.utility_objects_map[second_utility_name].get_top() + (self.spacing / 4) * UP)
+                    line.set_stroke(WHITE, width=2, opacity=0.4)
+
+                    self.lines_map[first_utility_name][second_utility_name] = line
                     current_lines.append(line)
 
                 self.play(*[ShowCreation(cl) for cl in current_lines])
+
+    def simulate_process(self, category_index: int, prev_utility_name: str | None) -> None:
+        if category_index > len(self.categories_data) - 1:
+            return
+
+        category_name = self.categories_data[category_index][0]
+        for utility_name in self.utilities_by_category[category_name]:
+            line = None
+
+            if prev_utility_name:
+                line = self.lines_map[prev_utility_name][utility_name]
+
+                self.bring_to_front(line)
+                self.play(line.animate.set_stroke("#3bff00", width=3, opacity=1), run_time=0.1)
+                self.wait(0.2)
+
+            self.simulate_process(category_index + 1, utility_name)
+
+            if prev_utility_name:
+                self.bring_to_back(line)
+                line.set_stroke(WHITE, width=2, opacity=0.4)
