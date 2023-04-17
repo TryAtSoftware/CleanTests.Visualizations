ce2a4e176094e4f0ead8d8be9a9ef3036c785b56
Tony Troeff <tonyslav_troeff@outlook.com>
2023-04-17 16:20:19 +0300

    Enhanced the simulation ny demonstrating how external demands work

--- standard_process.py
+++ standard_process.py
@@ -4,10 +4,10 @@ from manimlib import *
 class UtilitiesInCategories(Scene):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.spacing = 0.1
-        self.circle_radius = 0.3
+        self.spacing = 0.15
+        self.circle_radius = 0.5
         self.top = 3
-        self.left = -2
+        self.left = -2.5
         self.wait_time = 0.2
 
         self.utility_objects_map = {}
@@ -16,13 +16,18 @@ class UtilitiesInCategories(Scene):
         self.lines_map = {}
 
         self.categories_data = [("A", 3, RED), ("B", 2, YELLOW), ("C", 4, PURPLE), ("D", 3, GREEN)]
+        self.incompatible_utilities = {}
 
     def construct(self):
         for i, category_data in enumerate(self.categories_data):
             self.render_category(i, category_data[0], category_data[1], category_data[2])
 
         self.render_connections()
-        self.simulate_process(0, None)
+        self.simulate_process()
+
+        self.incompatible_utilities["A1"] = ["B2", "C1"]
+        self.incompatible_utilities["B2"] = ["D1", "D2"]
+        self.simulate_process()
 
     def render_category(self, index: int, name: str, utilities_count: int, color: Color):
         category_surface = Rectangle(
@@ -62,12 +67,14 @@ class UtilitiesInCategories(Scene):
         self.play(ShowCreation(utility), Write(utility_label))
 
     def render_connections(self):
+        for utility_name in self.utility_objects_map:
+            self.lines_map[utility_name] = {}
+
         for i in range(len(self.categories_data) - 1):
             first_category_name = self.categories_data[i][0]
             second_category_name = self.categories_data[i + 1][0]
 
             for first_utility_name in self.utilities_by_category[first_category_name]:
-                self.lines_map[first_utility_name] = {}
                 current_lines = []
 
                 for second_utility_name in self.utilities_by_category[second_category_name]:
@@ -75,27 +82,70 @@ class UtilitiesInCategories(Scene):
                     line.set_stroke(WHITE, width=2, opacity=0.4)
 
                     self.lines_map[first_utility_name][second_utility_name] = line
+                    self.lines_map[second_utility_name][first_utility_name] = line
                     current_lines.append(line)
 
                 self.play(*[ShowCreation(cl) for cl in current_lines])
 
-    def simulate_process(self, category_index: int, prev_utility_name: str | None) -> None:
-        if category_index > len(self.categories_data) - 1:
-            return
+    def simulate_process(self) -> None:
+        utilities_to_ignore = dict()
 
-        category_name = self.categories_data[category_index][0]
-        for utility_name in self.utilities_by_category[category_name]:
-            line = None
+        def simulate_process(category_index: int, prev_utility_name: str | None) -> None:
+            if category_index > len(self.categories_data) - 1:
+                return
 
-            if prev_utility_name:
-                line = self.lines_map[prev_utility_name][utility_name]
+            category_name = self.categories_data[category_index][0]
+            for utility_name in self.utilities_by_category[category_name]:
+                if utility_name in utilities_to_ignore and utilities_to_ignore[utility_name] > 0:
+                    continue
 
-                self.bring_to_front(line)
-                self.play(line.animate.set_stroke("#3bff00", width=3, opacity=1), run_time=0.1)
-                self.wait(0.2)
+                line = None
 
-            self.simulate_process(category_index + 1, utility_name)
+                if prev_utility_name:
+                    line = self.lines_map[prev_utility_name][utility_name]
 
-            if prev_utility_name:
-                self.bring_to_back(line)
-                line.set_stroke(WHITE, width=2, opacity=0.4)
+                    self.bring_to_front(line)
+                    self.play(line.animate.set_stroke("#3bff00", width=3, opacity=1), run_time=0.1)
+                    self.wait(0.2)
+
+                if utility_name in self.incompatible_utilities:
+                    for incompatible_utility_name in self.incompatible_utilities[utility_name]:
+                        if incompatible_utility_name not in utilities_to_ignore:
+                            utilities_to_ignore[incompatible_utility_name] = 0
+                        utilities_to_ignore[incompatible_utility_name] += 1
+
+                    self.hide_utilities(self.incompatible_utilities[utility_name], run_time=0.5)
+
+                simulate_process(category_index + 1, utility_name)
+
+                if utility_name in self.incompatible_utilities:
+                    for incompatible_utility_name in self.incompatible_utilities[utility_name]:
+                        utilities_to_ignore[incompatible_utility_name] -= 1
+
+                    self.show_utilities(self.incompatible_utilities[utility_name], run_time=0.5)
+
+                if prev_utility_name:
+                    self.bring_to_back(line)
+                    line.set_stroke(WHITE, width=2, opacity=0.4)
+
+        simulate_process(0, None)
+
+    def hide_utilities(self, utilities, **kwargs):
+        fade_out_animations = []
+
+        for utility_name in utilities:
+            fade_out_animations.append(FadeOut(self.utility_objects_map[utility_name]))
+            for line in self.lines_map[utility_name].values():
+                fade_out_animations.append(FadeOut(line))
+
+        self.play(*fade_out_animations, **kwargs)
+
+    def show_utilities(self, utilities, **kwargs):
+        fade_in_animations = []
+
+        for utility_name in utilities:
+            fade_in_animations.append(FadeIn(self.utility_objects_map[utility_name]))
+            for line in self.lines_map[utility_name].values():
+                fade_in_animations.append(FadeIn(line))
+
+        self.play(*fade_in_animations, **kwargs)
